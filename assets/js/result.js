ae434591f1a731661a808f8192ecfab430583461
b-chae <cbr5964@naver.com>
2021-01-12 20:41:45 +0900

    Add NodeJs & finish up structure
    Signed-off-by: b-chae <cbr5964@naver.com>

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,15 +1,15 @@
 var MBTI = ''
 const Title = document.querySelector(".title")
 const Ex = document.querySelector(".ex")
+const Wx = document.querySelector(".wx")
 
 function showResult(){
     Title.innerText = R[MBTI].b
     Ex.innerText = R[MBTI].e
+    Wx.innerText = R[MBTI].w
 }
 
 function init(){
-    temp = location.href.split('?')
-    MBTI = temp[1]
     showResult();
 }
 
